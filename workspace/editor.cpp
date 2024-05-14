ae4822fd8dbfc51db9a3dda4fc251fefb33508a5
Vescrity <vescrity@foxmail.com>
2024-05-14 10:21:32 +0800

    Status bar & File save

--- workspace/editor.cpp
+++ workspace/editor.cpp
@@ -1,6 +1,6 @@
 #include "editor.h"
 #include "ui_editor.h"
-
+#include <QFileDialog>
 Editor::Editor(QWidget *parent, const NoteList &l)
     : QMainWindow(parent), ui(new Ui::Editor), current_list(l)
 {
@@ -11,6 +11,9 @@ Editor::Editor(QWidget *parent, const NoteList &l)
         current_list.set_bpm(100);
     }
     ui->bpmSpinBox->setValue(current_list.get_bpm());
+    numerator = ui->numeratorSpinBox->value();
+    power2 = ui->power2Box->currentIndex();
+    ui->statusBar->showMessage(tr("初始化已完成"));
 }
 
 Editor::~Editor()
@@ -25,7 +28,14 @@ void Editor::set_list(const NoteList &l)
 
 void Editor::on_actionMidi_triggered()
 {
-    current_list.save_midi("temp.mid", numerator, power2);
+    QString fname=QFileDialog::getSaveFileName(this,tr("保存文件"),QDir::currentPath(),tr("MIDI 文件(*.mid)"));
+    if(!fname.isEmpty()){
+        current_list.save_midi(fname.toStdString(), numerator, power2);
+        ui->statusBar->showMessage(tr("文件已导出"));
+    }
+    else{
+        ui->statusBar->showMessage(tr("导出已取消"));
+    }
 }
 
 void Editor::on_power2Box_currentIndexChanged(int index)
