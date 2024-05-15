9afe76433d6021cabee2a0bd062cabe8e98f8ed0
Vescrity <vescrity@foxmail.com>
2024-05-15 20:48:57 +0800

    v0.1.0

--- workspace/about.cpp
+++ workspace/about.cpp
@@ -13,6 +13,9 @@ void Editor::on_actionAboutThis_triggered()
           "<h1>%1</h1>"
           "<p>Version %2.%3.%4</p>"
           "<p>编译于 %5 %6</p>"
+          "<p>许可：GPL-3.0 License</p>"
+          "<p>此程序是自由软件，您可以以自由软件基金会发布的 GNU通用公共许可协议第三版或(您可以选择)更高版方式重新发布它和/或修改它。</p>"
+          "<p>此程序是希望其会有用而发布，但没有任何担保</p>"
           "<p>更多信息请参考 <a href='https://github.com/Vescrity/MeloNote'>Github</a></p>")
           .arg(tr("MeloNote"))
           .arg(VERSION_1)
--- workspace/version.h
+++ workspace/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 #define VERSION_1 0
-#define VERSION_2 0
-#define VERSION_3 9
+#define VERSION_2 1
+#define VERSION_3 0
 
 #endif // VERSION_H
